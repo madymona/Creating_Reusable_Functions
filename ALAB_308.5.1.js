457aa502672abe30104a080a81a135291e18d93f
madymona <madymona@gmail.com>
2024-05-29 18:11:36 -0400

    Part 2

--- ALAB_308.5.1.js
+++ ALAB_308.5.1.js
@@ -2,47 +2,47 @@
 
 //Take an array of numbers and return the sum
 
-let randomNum = [13, 45, 97, 33, 78]
+// let randomNum = [13, 45, 97, 33, 78]
 
-function sumArray(numbers) {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i]
-    }
-    return sum
-}
-console.log(sumArray(randomNum))
+// function sumArray(numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i]
+//     }
+//     return sum
+// }
+// console.log(sumArray(randomNum))
 
-//Take an array of numbers and return the average.
+// //Take an array of numbers and return the average.
 
-function averageArray(numbers) {
-    const sum = sumArray(randomNum)
-    return sum / numbers.length
-}
-console.log(averageArray(randomNum))
+// function averageArray(numbers) {
+//     const sum = sumArray(randomNum)
+//     return sum / numbers.length
+// }
+// console.log(averageArray(randomNum))
 
-//Take an array of strings and return the longest string.
+// //Take an array of strings and return the longest string.
 
-let vegetables = ["carrot", "broccoli", "spinach", "cauliflower", "pepper", "tomato"]
+// let vegetables = ["carrot", "broccoli", "spinach", "cauliflower", "pepper", "tomato"]
 
-function findLongestVegetable(vegetables) {
-    let longestVeggie = ''
-    for (let i = 0; i < vegetables.length; i++) {
-        if (vegetables[i].length > longestVeggie.length) {
-            longestVeggie = vegetables[i]
-        }
-    }
-    return longestVeggie;
-}
+// function findLongestVegetable(vegetables) {
+//     let longestVeggie = ''
+//     for (let i = 0; i < vegetables.length; i++) {
+//         if (vegetables[i].length > longestVeggie.length) {
+//             longestVeggie = vegetables[i]
+//         }
+//     }
+//     return longestVeggie;
+// }
 
-console.log(findLongestVegetable(vegetables))
+// console.log(findLongestVegetable(vegetables))
 
-//Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+// //Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 
-function longerThanNumber(array, number) {
-    return array.filter(arr => arr.length > number);
-}
-console.log(longerThanNumber(vegetables, 6))
+// function longerThanNumber(array, number) {
+//     return array.filter(arr => arr.length > number);
+// }
+// console.log(longerThanNumber(vegetables, 6))
 
 //Take a number, n, and print every number between 1 and n without using loops. Use recursion.
 let n = 10
@@ -51,19 +51,62 @@ function recurse(n) {
         return
     }
     recurse(n - 1)
-    console.log(n)
+    if (n > 1) { 
+        console.log(n)
+    }
 }
-console.log(recurse(n))
+recurse(n)
 
 
 //Part 2: Thinking Methodically
 
-// Sort the array by age.
-// Filter the array to remove entries with an age greater than 50.
-// Map the array to change the “occupation” key to “job” and increment every age by 1.
-// Use the reduce method to calculate the sum of the ages.
-// Then use the result to calculate the average age.
+let peopleInfo = [
+    { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+];
 
+// Sort the array by age.
+
+function compareByAge(a, b) {
+    return parseInt(a.age) - parseInt(b.age);
+}
+peopleInfo = peopleInfo.sort(compareByAge)
+console.log(peopleInfo)
+
+// Filter the array to remove entries with an age greater than 50.
+let isGreaterThan50  = peopleInfo.filter(function(person) {
+    return parseInt(person.age) <= 50
+})
+
+console.log(isGreaterThan50)
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+
+let updateInfo = peopleInfo.map(function(person) {
+    return {
+        id: person.id,
+        name: person.name,
+        job: person.occupation, 
+        age: (parseInt(person.age)+ 1).toString()
+    };   
+});
+
+console.log(updateInfo);
+
+
+// Use the reduce method to calculate the sum of the ages.
+let sumOfAge = peopleInfo.reduce(function(accumulator, person) {
+    return accumulator + parseInt(person.age)
+}, 0)
+console.log(sumOfAge)
+
+// Then use the result to calculate the average age.
+function averageAge(age) {
+    return sumOfAge / age.length
+}
+console.log(averageAge(peopleInfo))
 
 
 // Part 3 Thinking Critically   
