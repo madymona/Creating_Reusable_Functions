60bbfce650e038bb31789e8cf405149b6e721933
madymona <madymona@gmail.com>
2024-05-29 14:56:12 -0400

    Part 1

--- ALAB_308.5.1.js
+++ ALAB_308.5.1.js
@@ -1,6 +1,9 @@
 //Part 1: Thinking Functionally
 
 //Take an array of numbers and return the sum
+
+let randomNum = [13, 45, 97, 33, 78]
+
 function sumArray(numbers) {
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
@@ -8,12 +11,49 @@ function sumArray(numbers) {
     }
     return sum
 }
-console.log(sumArray([13, 45, 97, 33, 78]))
+console.log(sumArray(randomNum))
 
 //Take an array of numbers and return the average.
+
+function averageArray(numbers) {
+    const sum = sumArray(randomNum)
+    return sum / numbers.length
+}
+console.log(averageArray(randomNum))
+
 //Take an array of strings and return the longest string.
+
+let vegetables = ["carrot", "broccoli", "spinach", "cauliflower", "pepper", "tomato"]
+
+function findLongestVegetable(vegetables) {
+    let longestVeggie = ''
+    for (let i = 0; i < vegetables.length; i++) {
+        if (vegetables[i].length > longestVeggie.length) {
+            longestVeggie = vegetables[i]
+        }
+    }
+    return longestVeggie;
+}
+
+console.log(findLongestVegetable(vegetables))
+
 //Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+
+function longerThanNumber(array, number) {
+    return array.filter(arr => arr.length > number);
+}
+console.log(longerThanNumber(vegetables, 6))
+
 //Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+let n = 10
+function recurse(n) {
+    if (n <= 1) {
+        return
+    }
+    recurse(n - 1)
+    console.log(n)
+}
+console.log(recurse(n))
 
 
 //Part 2: Thinking Methodically
