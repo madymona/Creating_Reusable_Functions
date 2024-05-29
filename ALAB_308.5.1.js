c63fcecd6fe996387b1aa0c2fe09baa5553e0fe3
madymona <madymona@gmail.com>
2024-05-29 19:26:19 -0400

    Final commit

--- ALAB_308.5.1.js
+++ ALAB_308.5.1.js
@@ -2,47 +2,47 @@
 
 //Take an array of numbers and return the sum
 
-// let randomNum = [13, 45, 97, 33, 78]
+let randomNum = [13, 45, 97, 33, 78]
 
-// function sumArray(numbers) {
-//     let sum = 0;
-//     for (let i = 0; i < numbers.length; i++) {
-//         sum += numbers[i]
-//     }
-//     return sum
-// }
-// console.log(sumArray(randomNum))
+function sumArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+    return sum
+}
+console.log(sumArray(randomNum))
 
-// //Take an array of numbers and return the average.
+//Take an array of numbers and return the average.
 
-// function averageArray(numbers) {
-//     const sum = sumArray(randomNum)
-//     return sum / numbers.length
-// }
-// console.log(averageArray(randomNum))
+function averageArray(numbers) {
+    const sum = sumArray(randomNum)
+    return sum / numbers.length
+}
+console.log(averageArray(randomNum))
 
-// //Take an array of strings and return the longest string.
+//Take an array of strings and return the longest string.
 
-// let vegetables = ["carrot", "broccoli", "spinach", "cauliflower", "pepper", "tomato"]
+let vegetables = ["carrot", "broccoli", "spinach", "cauliflower", "pepper", "tomato"]
 
-// function findLongestVegetable(vegetables) {
-//     let longestVeggie = ''
-//     for (let i = 0; i < vegetables.length; i++) {
-//         if (vegetables[i].length > longestVeggie.length) {
-//             longestVeggie = vegetables[i]
-//         }
-//     }
-//     return longestVeggie;
-// }
+function findLongestVegetable(vegetables) {
+    let longestVeggie = ''
+    for (let i = 0; i < vegetables.length; i++) {
+        if (vegetables[i].length > longestVeggie.length) {
+            longestVeggie = vegetables[i]
+        }
+    }
+    return longestVeggie;
+}
 
-// console.log(findLongestVegetable(vegetables))
+console.log(findLongestVegetable(vegetables))
 
-// //Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+//Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 
-// function longerThanNumber(array, number) {
-//     return array.filter(arr => arr.length > number);
-// }
-// console.log(longerThanNumber(vegetables, 6))
+function longerThanNumber(array, number) {
+    return array.filter(arr => arr.length > number);
+}
+console.log(longerThanNumber(vegetables, 6))
 
 //Take a number, n, and print every number between 1 and n without using loops. Use recursion.
 let n = 10
@@ -95,14 +95,15 @@ let updateInfo = peopleInfo.map(function(person) {
 
 console.log(updateInfo);
 
-
 // Use the reduce method to calculate the sum of the ages.
+
 let sumOfAge = peopleInfo.reduce(function(accumulator, person) {
     return accumulator + parseInt(person.age)
 }, 0)
 console.log(sumOfAge)
 
 // Then use the result to calculate the average age.
+
 function averageAge(age) {
     return sumOfAge / age.length
 }
@@ -115,3 +116,29 @@ console.log(averageAge(peopleInfo))
 Take an object and increment its age field.
 Take an object, make a copy, and increment the age field of the copy. Return the copy.
 For each of the functions above, if the object does not yet contain an age field, create one and set it to 0. Also, add (or modify, as appropriate) an updated_at field that stores a Date object with the current time.*/
+
+let person = {
+    name:"Madina",
+    age:0,
+    occupation:"baby"
+
+}
+
+function incrementAge (number) {
+    person.age += 1
+    person.updated_at = new Date();
+    return person.age;
+}
+
+let newAge = incrementAge(person);
+console.log(person)
+
+function copyAndIncrementAge(person) {
+    let newPerson = { ...person }
+    newPerson.age += 1
+    newPerson.occupation ="toddler"
+    return newPerson
+}
+
+let newPerson = copyAndIncrementAge(person);
+console.log(newPerson);
